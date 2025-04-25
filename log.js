7d8caf0ef7b33d9c2bbea0a907496652a63c32b7
Paul Wareing <paul.wareing@prometheanworld.com>
2025-04-25 16:17:37 +0100

    Adding width, height & pressure fields

--- log.js
+++ log.js
@@ -1,9 +1,11 @@
 const logDiv = document.getElementById('log');
 
 function logEvent(event) {
-  const { type, clientX, clientY, pointerType } = event;
+  const { type, clientX, clientY, pointerType, width, height, pressure } = event;
   const pointerInfo = pointerType ? ` [pointerType: ${pointerType}]` : '';
-  const logEntry = `${new Date().toLocaleTimeString()} - ${type} at (${Math.round(clientX)}, ${Math.round(clientY)})${pointerInfo}\n`;
+  const sizeInfo = width && height ? ` [size: ${Math.round(width)}x${Math.round(height)}]` : '';
+  const pressureInfo = pressure !== undefined ? ` [pressure: ${pressure.toFixed(2)}]` : '';
+  const logEntry = `${new Date().toLocaleTimeString()} - ${type} at (${Math.round(clientX)}, ${Math.round(clientY)})${pointerInfo}${sizeInfo}${pressureInfo}\n`;
   logDiv.textContent += logEntry;
   logDiv.scrollTop = logDiv.scrollHeight;
 }
@@ -15,9 +17,11 @@ const eventsToLog = ['pointerdown', 'pointermove', 'pointerup', 'pointercancel']
 eventsToLog.forEach(eventType => {
   document.addEventListener(eventType, event => {
     if (event.touches && event.touches.length > 0) {
-      logEvent({ type: event.type, clientX: event.touches[0].clientX, clientY: event.touches[0].clientY });
+      const touch = event.touches[0];
+      logEvent({ type: event.type, clientX: touch.clientX, clientY: touch.clientY, width: touch.radiusX * 2, height: touch.radiusY * 2, pressure: touch.force });
     } else if (event.changedTouches && event.changedTouches.length > 0) {
-      logEvent({ type: event.type, clientX: event.changedTouches[0].clientX, clientY: event.changedTouches[0].clientY });
+      const touch = event.changedTouches[0];
+      logEvent({ type: event.type, clientX: touch.clientX, clientY: touch.clientY, width: touch.radiusX * 2, height: touch.radiusY * 2, pressure: touch.force });
     } else {
       logEvent(event);
     }
