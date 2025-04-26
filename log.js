b07fdc07110e5ffd449d940059cd815e65b88623
Paul Wareing <paul.wareing@prometheanworld.com>
2025-04-26 13:25:26 +0100

    Add support for multiple concurrent contacts

--- log.js
+++ log.js
@@ -17,11 +17,16 @@ const eventsToLog = ['pointerdown', 'pointermove', 'pointerup', 'pointercancel']
 eventsToLog.forEach(eventType => {
   document.addEventListener(eventType, event => {
     if (event.touches && event.touches.length > 0) {
-      const touch = event.touches[0];
-      logEvent({ type: event.type, clientX: touch.clientX, clientY: touch.clientY, width: touch.radiusX * 2, height: touch.radiusY * 2, pressure: touch.force });
-    } else if (event.changedTouches && event.changedTouches.length > 0) {
-      const touch = event.changedTouches[0];
-      logEvent({ type: event.type, clientX: touch.clientX, clientY: touch.clientY, width: touch.radiusX * 2, height: touch.radiusY * 2, pressure: touch.force });
+      Array.from(event.touches).forEach(touch => {
+        logEvent({
+          type: event.type,
+          clientX: touch.clientX,
+          clientY: touch.clientY,
+          width: touch.radiusX * 2,
+          height: touch.radiusY * 2,
+          pressure: touch.force
+        });
+      });
     } else {
       logEvent(event);
     }
